ca6e5bb018b57b7d7b5a8f8bd2736f368670ff52
Sandro Campos <scampos@andrew.cmu.edu>
2024-06-05 16:42:36 -0400

    Pack tests into methods

--- tests/gundam/test_example_redblue.py
+++ tests/gundam/test_example_redblue.py
@@ -9,16 +9,17 @@ fred = "tests/data/red_blue_all/red.cnt"
 fblue = "tests/data/red_blue_all/blue.cnt"
 fall = "tests/data/red_blue_all/all.cnt"
 
-# MAKE COMPARISON CF PLOT (no need to read .cnt explicitly)
-f, ax1, ax2 = gun.comparecf([fred, fblue, fall], [fall], plotratio=True, fill=True)
 
-# NOW READ CORRELATIONS
-red = gun.readcounts(fred)
-blue = gun.readcounts(fblue)
+def test_example_redblue():
+    # MAKE COMPARISON CF PLOT (no need to read .cnt explicitly)
+    f, ax1, ax2 = gun.comparecf([fred, fblue, fall], [fall], plotratio=True, fill=True)
 
-# FIT POWER LAW TO RED AND BLUE GALAXIES BETWEEN 0.1-10 Mpc
-# plt.sca(ax1)
-gun.fitpowerlaw(red.rpm, red.wrp, red.wrperr, fitrange=[0.1, 10.0], plot=True)
-gun.fitpowerlaw(blue.rpm, blue.wrp, blue.wrperr, fitrange=[0.1, 10.0], plot=True)
+    # NOW READ CORRELATIONS
+    red = gun.readcounts(fred)
+    blue = gun.readcounts(fblue)
 
-# plt.show()
+    # FIT POWER LAW TO RED AND BLUE GALAXIES BETWEEN 0.1-10 Mpc
+    # plt.sca(ax1)
+    gun.fitpowerlaw(red.rpm, red.wrp, red.wrperr, fitrange=[0.1, 10.0], plot=True)
+    gun.fitpowerlaw(blue.rpm, blue.wrp, blue.wrperr, fitrange=[0.1, 10.0], plot=True)
+    # plt.show()
